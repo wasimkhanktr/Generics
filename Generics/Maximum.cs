463675b501429545f542ead43e9c572fd55b353f
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-30 00:15:05 +0530

    [Wasim] Add: Generic Refector 1

---
+++ Generics/Maximum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Maximum<T>
+    {
+        public T FindMax(T a, T b, T c)
+        {
+            if (Comparer<T>.Default.Compare(a, b) > 0 && Comparer<T>.Default.Compare(a, c) > 0)
+                return a;
+            else if (Comparer<T>.Default.Compare(b, c) > 0 && Comparer<T>.Default.Compare(b, c) > 0)
+                return b;
+            else
+                return c;
+        }
+
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maximum<int> maximum = new Maximum<int>();
+            int max = maximum.FindMax(1, 2, 3);
+            Maximum<string> maximum1 = new Maximum<string>();
+            string max1 = maximum1.FindMax("Apple", "Banana", "Peach");
+            Console.WriteLine(max);
+            Console.WriteLine(max1);
+        }
+    }
+}
